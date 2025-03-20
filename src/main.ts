767dfc06ed76909924c8e62be30bff353576691f
gwiyeomgo <gwiyeomgo@gmail.com>
2025-03-21 00:36:51 +0900

    #5 NestJS v9 이상 Logger 직접 확장 금지로 대신 ConsoleLogger 사용

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  //유효성 검사
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  //CORS 활성화
   app.enableCors();
 
   await app.listen(3000);
